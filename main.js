c4ad0825acf13a058a9d0c54b1f4c19e3c842a12
Marta Szewczul <marta.szewczul@gmail.com>
2022-11-24 13:52:13 +0100

    js element

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const name='Marta Szewczul'
 console.log(`Cześć! Nazywam się ${name} i witam Cię na mojej stronie!`);
 
 
+const motto = document.querySelector('.motto--js');
+console.log(motto)
+
+motto.innerHTML = `Coco Jamboo i do przodu!`;
