0f972d1d6614822e6d635fd39a20e77ea6a5908f
Marta Szewczul <marta.szewczul@gmail.com>
2022-11-30 11:44:49 +0100

    js greet function

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ const motto = document.querySelector('.motto--js');
 console.log(motto)
 
 motto.innerHTML = `Coco Jamboo i do przodu!`;
+
+function greet(name, age) {
+console.log (`Cześć! Nazywam się ${name} i mam ${age} lat`)
+};
+
+greet(name, 39)
